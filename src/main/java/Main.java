9f2df90d173195fa838f938b77230d4c8cf83576
Monika Skoczylas <monika@iine.io>
2024-12-14 16:49:04 +0100

    Pierwszy commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,142 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("It works!");
+//        byte value1 = 3;
+//        byte value2 = 127;
+//        System.out.println("Zmienna value1 to " + value1 + " a value2 to " + value2);
+//        int mojazmienna;  //deklaracja
+//        int value3 = 4;   //inicjacja
+//        int value4 = 5; // definicja
+//        byte zm = (byte) 129;
+//        System.out.println("Wartość po rzutowaniu to " + zm);
+
+//        int valueToCheck = 128;
+//        if (valueToCheck <= 127){
+//            System.out.println("The number type is byte.");
+//        }
+
+//        int valueToCheck = 128;
+//        if (valueToCheck <= 127){
+//            System.out.println("The number type is byte.");
+//        }
+//        else {
+//            System.out.println("The number type is int.");
+//        }
+
+//        int temp = -3;
+//        if (temp < 0) {
+//            System.out.println("Jest zimno weź czapkę");
+//        } else {
+//            System.out.println("Jest cieplo");
+//        }
+//
+//        String znaki = "Dowolne znaki :/";
+//        System.out.println(znaki);
+
+//        char nowyTyp = 'c';
+//        System.out.println(nowyTyp);
+//
+//        char nowyTyp2 = '%';
+//        System.out.println(nowyTyp2);
+//
+//        char nowyTyp3 = 123;
+//        System.out.println(nowyTyp3);
+//
+//        System.out.println(nowyTyp + ", " + nowyTyp2 + ", " + nowyTyp3);
+
+//
+//        String kolor = "niebieski";
+//        if (kolor == "zielony")     // == operator porównania, = operator przypisania
+//        {
+//            System.out.println("jedź");
+//        } else if (kolor == "czerwony") {
+//            System.out.println("Stój");
+//        }
+//        else {
+//            System.out.println("nieokreślony sygnał");
+//        }
+
+
+//        int mojaZmienna = 1;
+//        System.out.println(mojaZmienna);
+//
+//        mojaZmienna = mojaZmienna + 1;
+//        System.out.println(mojaZmienna);
+//
+//        mojaZmienna++;
+//        System.out.println(mojaZmienna);
+//
+//        mojaZmienna += 1;
+//        System.out.println(mojaZmienna);
+//
+//        mojaZmienna += 10;
+//        System.out.println(mojaZmienna);
+//
+//        int zm = 5;
+//        System.out.println(++zm);
+
+//        int zmiennaGlobalna = 90;
+//
+//        while (zmiennaGlobalna > 0) {
+//            zmiennaGlobalna==;
+//
+//            int zmiennaLokalna = 0;
+//        }
+//
+//        zmiennaLokalna++;
+
+
+/*
+komentarz
+wielolinijkowy
+ */
+
+//        float floatVariable = 4.3232F;
+//        double doubleVariable = 6.34563222;
+
+//        for (int licznik = 0; licznik <=10; licznik++) {
+//            System.out.println("Napis po raz " + licznik);
+//        }
+//
+//        int liczba = 11;
+
+//        while (liczba <=10) {
+//            System.out.println("liczba to " + liczba);
+//            liczba++;
+//        }
+
+//        do {
+//            System.out.println(liczba);
+//            liczba++;
+//        } while (liczba <=10);
+
+//        int numberToCheck = 101;
+//
+//        if (numberToCheck %2 == 0) {
+//            System.out.println("Liczba jest parzysta");
+//        }
+//        else {
+//            System.out.println("Liczba jest nieparzysta");
+//        }
+
+
+//        int nowaLiczba = 77;
+//        System.out.println(nowaLiczba%33);
+//
+//        double nowaLiczba1 = 77;
+//        System.out.println(nowaLiczba1/33);
+
+
+        boolean sygnalizacja = true;
+        boolean zapory = false;
+        if (sygnalizacja && zapory){
+            System.out.println("Można jechać");
+        }
+        else {
+            System.out.println("Nie można jechać");
+        }
+
+    }
+}
